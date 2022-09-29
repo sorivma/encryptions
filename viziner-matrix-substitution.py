e7e2bc1b417071c04202d515dc52535afba0f677
sorivma <sorivma03.06@gmail.com>
2022-09-29 20:48:23 +0300

    29.09 update - 4

--- viziner-matrix-substitution.py
+++ viziner-matrix-substitution.py
@@ -1,6 +1,7 @@
 from alphabets import k_alphabet
 from main import main_func
-
+from utils import shift_alphabet
+from prettytable import PrettyTable
 INFO = """
     //WORK IN PROGRESS//
 """
@@ -11,14 +12,23 @@ TEST_MESSAGE = "Никакой транспорт не будет попутны
 def encrypt(message, alphabet, seed):
     counter = 0
     encryption = ""
+    matrix = PrettyTable()
+    matrix.title = "Сокращенная матрица Вижинера"
+    matrix.field_names = [letter for letter in alphabet]
     for letter in message:
         encryption += alphabet[(alphabet.index(letter) + alphabet.index(seed[counter % len(seed)])) % len(alphabet)]
-        counter += 1
+        counter+=1
+
+    for i in range(len(seed)):
+        matrix.add_row(shift_alphabet(alphabet, alphabet.index(seed[i % len(seed)]) % len(alphabet)))
+
+    print(matrix)
+
     return encryption
 
 
 def decrypt(message, alphabet, seed):
-    counter = 0
+    counter = -1
     decryption = ""
     for letter in message:
         decryption += alphabet[(alphabet.index(letter) - alphabet.index(seed[counter % len(seed)])) % len(alphabet)]
